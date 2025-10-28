1ffa34a8e7ce7b6ba79cea912bba91415639d063
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-10-28 17:13:53 -0600

    feat: añade metodo PUT y DELETE a la api

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -5,7 +5,7 @@ interface Params {
     id: number
 }
 
-export const GET = async ( request: Request, { params }: { params: Params }) => {
+export const GET = async (request: Request, { params }: { params: Params }) => {
     const { id } = await params
     const oneTask = await prisma.task.findUnique({
         where: {
@@ -15,17 +15,27 @@ export const GET = async ( request: Request, { params }: { params: Params }) => 
     return NextResponse.json({ oneTask })
 }
 
-export const POST = async ( request: Request, { params }: { params: Params }) => {
+export const PUT = async (request: Request, { params }: { params: Params }) => {
     const { id } = await params;
-    return NextResponse.json({ message: `post one task ${id}` })
+    const { title, description } = await request.json();
+    const updatedTask = await prisma.task.update({
+        where: {
+            id: Number(id)
+        },
+        data: {
+            title,
+            description
+        }
+    })
+    return NextResponse.json(updatedTask)
 }
 
-export const PUT = async ( request: Request, { params }: { params: Params }) => {
+export const DELETE = async (request: Request, { params }: { params: Params }) => {
     const { id } = await params;
-    return NextResponse.json({ message: `put one task ${id}` })
-}
-
-export const DELETE = async ( request: Request, { params }: { params: Params }) => {
-    const { id } = await params;
-    return NextResponse.json({ message: `delete one task ${id}` })
+    const deleteTask = await prisma.task.delete({
+        where: {
+            id: Number(id)
+        }
+    })
+    return NextResponse.json(deleteTask)
 }
