6afc4a9a39d1d00c46b276ae66fc7e23cbb21a0e
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-11-01 18:42:16 -0600

    fix: cambia elemento anchor por Link de NextJS

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
+
 const fetchData = async () => {
   try {
     const response = await fetch("http://localhost:3000/api/tasks");
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.error(error);
@@ -11,15 +12,16 @@ const fetchData = async () => {
 
 const HomePage = async () => {
   const dataTask = await fetchData();
+
   return (
     <div>
       <h1 className="font-bold text-2xl text-center py-8">listado de tareas</h1>
       <div>
         {dataTask.map((task: any) => (
-          <div className="flex flex-col p-3 border border-slate-800 px-5 hover:bg-slate-700 transition-all duration-200" key={task.id}>
-            <h2 className="text-xl font-bold">{task.title}</h2>
+          <Link href={`/tasks/${task.id}`} className="flex flex-col p-3 border border-slate-800 px-5 hover:bg-slate-700 transition-all duration-200 cursor-pointer" key={task.id}>
+            <h2 className="text-xl font-bold">{task.id}.{task.title}</h2>
             <p className="text-sm">{task.description}</p>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
